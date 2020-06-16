2d09bda7935a7133162cc1638fc9e1b137787dec
paolocappelletti <paolo@horizenlabs.io>
2020-06-16 16:59:24 +0200

    removed sidechainid and vcl_ccout from serialization

--- test/transaction/transaction-sidechain.js
+++ test/transaction/transaction-sidechain.js
@@ -11,13 +11,13 @@ describe('#Sidechain creation', function() {
 
   describe('SC_create', function() {
     var index = 0;
-    txes.forEach(function(vector) {
+    txes.forEach(function(ele) {
 
       it('vector #' + index, function() {
-        if (vector.length > 1) {
-          var hexa = vector[0];
+
+          var hexa = ele.hex;
           var tx=Transaction(hexa);
-          var txJson = JSON.parse(vector[1]);
+          var txJson = ele.json;
           //version
           assert.equal(tx.version,txJson.version);
 
@@ -44,23 +44,18 @@ describe('#Sidechain creation', function() {
           //Sidechain
           var sc = {
             vsc_ccout : tx.sc_params.toObject().vsc_ccout,
-            vcl_ccout : tx.sc_params.toObject().vcl_ccout,
             vft_ccout : tx.sc_params.toObject().vft_ccout
           };
 
           //vsc_ccout
           assert.equal(sc.vsc_ccout.length,txJson.vsc_ccout.length);
           for (var i=0; i<txJson.vsc_ccout.length;i++) {
-            assert.equal(sc.vsc_ccout[i].scid,txJson.vsc_ccout[i].scid);
+            assert.equal(Transaction.calculateSidechainId(tx.hash, i), txJson.vsc_ccout[i].scid);
             assert.equal(sc.vsc_ccout[i].epoch_length,txJson.vsc_ccout[i]["withdrawal epoch length"]);
             assert.equal(sc.vsc_ccout[i].satoshis / 1e8,txJson.vsc_ccout[i].value);
             assert.equal(sc.vsc_ccout[i].address,txJson.vsc_ccout[i].address);
             assert.equal(sc.vsc_ccout[i].customData,txJson.vsc_ccout[i].customData);
           }
-
-          //vcl_ccout
-          assert.equal(sc.vcl_ccout.length,txJson.vcl_ccout.length);
-          assert.equal(txJson.vcl_ccout.length,0);
 
           //vft_ccout
           assert.equal(sc.vft_ccout.length,txJson.vft_ccout.length);
@@ -76,7 +71,7 @@ describe('#Sidechain creation', function() {
           //create transaction from existing transaction
           var new_Tx =  Transaction(tx.toObject());
           assert.equal(new_Tx.toBuffer().toString('hex'),tx.toBuffer().toString('hex'));
-        }
+
       });
       index++;
     });
