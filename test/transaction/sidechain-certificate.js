56a6f0072fd050fb08fdb780322d05571ece6453
MarcoOl94 <marco.olivero94@libero.it>
2021-07-22 15:28:27 +0200

    Added certificates

--- test/transaction/sidechain-certificate.js
+++ test/transaction/sidechain-certificate.js
@@ -1,170 +1,74 @@
 "use strict";
 
-var Address = require("../../lib/address");
-var Output = require("../../lib/transaction/output");
-var Input = require("../../lib/transaction/input/input");
 var Transaction = require("../../lib/transaction/transaction");
 var assert = require('chai').assert;
-var fs = require('fs');
-var testdataInput = require('../data_tx/tx-sidechain-certificate.js');
-var testdataExpected =  fs.readFileSync('test/data_tx/tx-sidechain-certificate.json').toString();
-var Networks = require('../../lib/networks');
-var BufferReader = require('../../lib/encoding/bufferreader');
+var testdataExpected =  require('../data_tx/tx-sidechain-certificate.json')
+var BufferUtil = require('../../lib/util/buffer');
 
 describe('#Sidechain certificate', function() {
 
   describe('Certificate parsing', function() {
-    var sccer = new Transaction(testdataInput.blockhex);
-    var sccerExpected = JSON.parse(testdataExpected);
+    var index = 0;
+    testdataExpected.forEach(function (ele) {
+      it('vector #' + index+": "+ele.description, function() {
+        var hexa = ele.hex;
+        var cert=Transaction(hexa);
+        var certJson = ele.json;
 
-
-    it('parses the sidechainId correctly', function() {
-      assert.equal(sccer.sidechainId, sccerExpected.cert.scid);
-    });
-    it('parses the epochNumber correctly', function() {
-      assert.equal(sccer.epochNumber, sccerExpected.cert.epochNumber);
-    });
-    it('parses the endEpochBlockHash correctly', function() {
-      assert.equal(sccer.endEpochBlockHash, sccerExpected.cert.endEpochBlockHash);
-    }); 
-    it('parses the version correctly', function() {
-      assert.equal(sccer.version, sccerExpected.version);
-    }); 
-    it('parses the quality correctly', function() {
-      assert.equal(sccer.quality, sccerExpected.cert.quality);
-    }); 
-    it('parses the quality correctly', function() {
-      assert.equal(sccer.scProof, sccerExpected.cert.scProof);
-    }); 
-    it('parses the getValueOfBackwardTransfers correctly', function() {
-      var expectedVout = sccerExpected.vout.filter((ele) => { return ele[ "backward transfer"]});
-      var expectedAmount = 0;
-      expectedVout.forEach(function(output) {
-        expectedAmount = expectedAmount +output.valueZat;
-      });
-       assert.equal(sccer.getValueOfBackwardTransfers(), expectedAmount);
-    }); 
-
-    describe('Certificate backwardTransfer output parsing', function() {
-      var expectedVout = sccerExpected.vout.filter((ele) => { return ele[ "backward transfer"]});
-      var parsedVout = sccer.outputs.filter((ele) =>  ele.isFromBackwardTransfer);
-
-      it('parses the output correctly', function() {       
-        assert.equal(parsedVout.length, expectedVout.length);
-        if (parsedVout.length > 0){
-          var output1 = parsedVout[0];
-          assert.equal(output1.toObject().pubKeyHash, expectedVout[0].pubkeyhash);
-          assert.equal(output1.satoshis, expectedVout[0].valueZat);
-          var address = Address(output1._pubKeyHash, Networks.regtest).toString(true);
-          assert.equal(address, expectedVout[0].scriptPubKey.addresses[0]);
+        //certid
+        assert.equal(cert.hash, certJson.certid);
+        //version
+        assert.equal(cert.version, certJson.version);
+        //vin
+        assert.equal(cert.inputs.length,certJson.vin.length);
+        for (var i=0; i<certJson.vin.length;i++) {
+          assert.equal(cert.inputs[i].toObject().prevTxId,certJson.vin[i].txid);
+          assert.equal(cert.inputs[i].toObject().sequenceNumber,certJson.vin[i].sequence);
+          assert.equal(cert.inputs[i].toObject().outputIndex,certJson.vin[i].vout);
         }
-      });
-      it('output to object', function() {
-        if (parsedVout.length > 0){
-          var output1 = parsedVout[0];
-          var obj = output1.toObject();
-          assert.equal(obj.pubKeyHash, expectedVout[0].pubkeyhash);
-          assert.equal(obj.satoshis, expectedVout[0].valueZat);
-        }
-      }); 
-      it('output  to raw and from raw', function() {
-        if (parsedVout.length > 0){
-          var output1 = parsedVout[0];
-          var buf = output1.toBufferWriter();    
-          var obj2 = Output.fromBufferReader(new BufferReader(buf.toBuffer()), true);
-          assert.equal(obj2.toObject().pubKeyHash, expectedVout[0].pubkeyhash);
-          assert.equal(obj2.satoshis, expectedVout[0].valueZat);
-        }
-      });  
-    }); 
 
-    describe('Certificate transaction output parsing', function() {
-      it('parses the output correctly', function() {       
-        assert.equal(sccer.outputs.length, sccerExpected.vout.length);
-        if (sccerExpected.vout.length > 0){
-          var output1 = sccer.outputs[0];
-          assert.equal(output1.pubKeyHash, sccerExpected.vout[0].pubkeyhash);
-          assert.equal(output1.satoshis, sccerExpected.vout[0].valueZat);
-          if (output1.script.isPublicKeyHashReplayOut()){
-            var address = Address(output1.script.getPublicKeyHash(), Networks.regtest).toString(true);
-            assert.equal(address, sccerExpected.vout[0].scriptPubKey.addresses[0]);
+        //scid
+        assert.equal(cert.sidechainId, certJson.cert.scid);
+        //epoch number
+        assert.equal(cert.epochNumber, certJson.cert.epochNumber);
+        //quality
+        assert.equal(cert.quality, certJson.cert.quality);
+        //endEpochCumScTxCommTreeRoot
+        assert.equal(cert.endEpochCumScTxCommTreeRoot.toString('hex'), certJson.cert.endEpochCumScTxCommTreeRoot);
+        //scProof
+        assert.equal(cert.scProof, certJson.cert.scProof);
+        //vFieldElementCertificateField
+        assert.deepEqual(cert.vFieldElementCertificateField,certJson.cert.vFieldElementCertificateField);
+        //vBitVectorCertificateField
+        assert.deepEqual(cert.vBitVectorCertificateField,certJson.cert.vBitVectorCertificateField);
+        //ftScFee
+        assert.equal(cert.ftScFee / 1e8, certJson.cert.ftScFee);
+        //mbtrScFee
+        assert.equal(cert.mbtrScFee / 1e8, certJson.cert.mbtrScFee);
+
+        //vout
+        assert.equal(cert.outputs.length,certJson.vout.length);
+        for (var i=0; i<certJson.vout.length;i++) {
+          assert.equal(cert.outputs[i].toObject().satoshis,certJson.vout[i].valueZat);
+          assert.equal(cert.outputs[i].toObject().script,certJson.vout[i].scriptPubKey.hex);
+          if (cert.outputs[i].toObject().isFromBackwardTransfer && cert.outputs[i].toObject().isFromBackwardTransfer == true) {
+            assert.equal(cert.outputs[i].toObject().isFromBackwardTransfer,certJson.vout[i]["backward transfer"]);
+            assert.equal(BufferUtil.reverse(Buffer.from(cert.outputs[i].toObject().pubKeyHash, 'hex')).toString('hex'),certJson.vout[i].pubkeyhash);
           }
         }
+
+        //serialize
+        assert.equal(cert.toBuffer().toString('hex'),hexa);
+
+        //create certificate from existing certificate
+        var new_Cert =  Transaction(cert.toObject());
+        assert.equal(new_Cert.toBuffer().toString('hex'),cert.toBuffer().toString('hex'));
       });
-      it('output to object', function() {
-        if (sccerExpected.vout.length > 0){
-          var output1 = sccer.outputs[0];
-          var obj = output1.toObject();
-          assert.equal(obj.pubKeyHash, output1.pubKeyHash);
-          assert.equal(obj.satoshis, sccerExpected.vout[0].valueZat);
-        }
-      }); 
-      it('output from object', function() {
-        if (sccerExpected.vout.length > 0){
-          var output1 = sccer.outputs[0];
-          var obj = output1.toObject();
-          var obj2 = new Output(obj);
-          assert.equal(obj2.pubKeyHash, sccerExpected.vout[0].pubkeyhash);
-          assert.equal(obj2.satoshis, sccerExpected.vout[0].valueZat);
-        }
-      }); 
-      it('output  to raw and from raw', function() {
-        if (sccerExpected.vout.length > 0){
-          var output1 = sccer.outputs[0];
-          var buf = output1.toBufferWriter();
-          var bRead = new BufferReader(buf.toBuffer());
-          var obj2 = Output.fromBufferReader(bRead);
-          assert.equal(obj2.pubKeyHash, sccerExpected.vout[0].pubkeyhash);
-          assert.equal(obj2.satoshis, sccerExpected.vout[0].valueZat);
-        }
-      });  
-    }); 
-
-
-    describe('Certificate transaction input parsing', function() {
-      it('parses the input correctly', function() {       
-        assert.equal(sccer.inputs.length, sccerExpected.vin.length);
-        if (sccer.inputs.length > 0){
-          var input1 = sccer.inputs[0];
-          assert.equal(input1.prevTxId.toString('hex'), sccerExpected.vin[0].txid);
-          assert.equal(input1.sequenceNumber, sccerExpected.vin[0].sequence);
-        }
-      });
-      it('output to object', function() {
-        if (sccer.inputs.length > 0){
-          var input1 = sccer.inputs[0];
-          var obj = input1.toObject();
-          assert.equal(obj.prevTxId, input1.prevTxId.toString('hex'));
-          assert.equal(obj.sequenceNumber, input1.sequenceNumber);
-        }
-      }); 
-      it('output from object', function() {
-        if (sccer.inputs.length > 0){
-          var input1 = sccer.inputs[0];
-          var obj = input1.toObject();
-          var obj2 = new Input(obj);
-          assert.equal(obj2.prevTxId.toString('hex'), input1.prevTxId.toString('hex'));
-          assert.equal(obj2.sequenceNumber, input1.sequenceNumber);
-        }
-      });         
-    }); 
-
-    it('certificate to raw', function() {
-      var buf = sccer.toBuffer();
-      var bufHex = buf.toString('hex');
-      assert.equal(bufHex, testdataInput.blockhex);
+      index++;
     });
 
-    it('certificate id (hash) is generated correclty', function() {
-      assert.equal(sccer.id, sccerExpected.certid);
-      assert.equal(sccer.hash, sccerExpected.certid); 
-    });
- 
-    it('test certificate outputs property', function() {
-      assert.equal(sccer.outputs.length, sccerExpected.vout.length);
-    });   
 
-  }); 
+  });
 
 
 });
